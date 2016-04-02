c161ac078648727b94ff44c12574255f08920639
HoneyOney <honey.zefirka@mail.ru>
2016-04-02 10:41:38 +0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,26 @@
-console.log('This would be the main JS file.');
+//JS samples
+function showHref() {
+    alert(location.href);
+}
+
+//BOM samples
+function showAgent() {
+    alert(navigator.userAgent);
+}
+
+//DOM samples
+function makeBGBlue() {
+    if (document.body.style.background.valueOf() === new String("blue").valueOf()) {
+        document.body.style.background = "";
+    } else {
+        document.body.style.background = "blue";
+    }
+}
+
+//jQuery
+
+$(document).ready(function() {
+    $("#fadeinout").click(function() {
+        $(this).fadeOut(1000).fadeIn(1000);
+    });
+});
