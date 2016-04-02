f8c8d0c364ee2f0af9ed8bc6e97d60f296da5299
MaxPolytsin <maxxx-129@yandex.ru>
2016-04-02 21:10:36 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,7 +9,7 @@ function showAgent() {
 }
 
 //DOM samples
-function makeBGRed() {
+function makeBGGreen() {
     if (document.body.style.background.valueOf() === new String("green").valueOf()) {
         document.body.style.background = "";
     } else {
@@ -18,22 +18,7 @@ function makeBGRed() {
 }
 
 //jQuery
-<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
-<script type='text/javascript' src='js/jquery.easing.1.2.js'></script>
-<script type='text/javascript' src='js/jquery.circulate.js'></script>
 
-$("#anything).circulate({
-
-    speed: 400,                  // Speed of each quarter segment of animation, 1000 = 1 second
-    height: 200,                 // Distance vertically to travel
-    width: 200,                  // Distance horizontally to travel
-    sizeAdjustment: 100,         // Percentage to grow or shrink
-    loop: false,                 // Circulate continuously
-    zIndexValues: [1, 1, 1, 1]   // Sets z-index value at each stop of animation
-
-});
-
-$("#anything").circulate("Stop");
 $(document).ready(function() {
     $("#fadeinout").click(function() {
         $(this).fadeOut(1000).fadeIn(1000);
