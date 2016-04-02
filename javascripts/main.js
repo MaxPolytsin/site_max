9f14ae1f55d27b4341e9a4ff04942d76126a903d
MaxPolytsin <maxxx-129@yandex.ru>
2016-04-02 21:11:22 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,7 +9,7 @@ function showAgent() {
 }
 
 //DOM samples
-function makeBGGreen() {
+function makeBGRed() {
     if (document.body.style.background.valueOf() === new String("green").valueOf()) {
         document.body.style.background = "";
     } else {
